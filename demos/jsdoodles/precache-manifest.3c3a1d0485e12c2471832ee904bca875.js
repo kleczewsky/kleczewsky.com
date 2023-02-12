a016e1d0485c7b21c784cd06165cf8ead0dbaf61
Eryk Kleczewski <MrNikoZero@gmail.com>
2023-02-12 19:37:26 +0100

    add webape, rickandbrowser, phyxlist, jsdoodles demos

---
+++ demos/jsdoodles/precache-manifest.3c3a1d0485e12c2471832ee904bca875.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc082826ba56bf5518bb8191b1a36afc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61fff2e98bdec9a004a0",
+    "url": "./static/css/main.00fc6fb1.chunk.css"
+  },
+  {
+    "revision": "8487d87007940f0be9f6",
+    "url": "./static/js/2.c58f4b94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c58f4b94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61fff2e98bdec9a004a0",
+    "url": "./static/js/main.69bcb2f2.chunk.js"
+  },
+  {
+    "revision": "854f02e85dc1cc4abe77",
+    "url": "./static/js/runtime-main.835a98b0.js"
+  },
+  {
+    "revision": "1139739e215daef52bb84c77f23b28d6",
+    "url": "./static/media/hourglass.1139739e.svg"
+  },
+  {
+    "revision": "80642e370b426d2488bce31529eb9fdc",
+    "url": "./static/media/kleczewskyOps.80642e37.svg"
+  },
+  {
+    "revision": "68996ca4ba0dbff74671368c1c180ec1",
+    "url": "./static/media/plane.68996ca4.svg"
+  },
+  {
+    "revision": "14f2b0337061b799bd71bebc9aeef8be",
+    "url": "./static/media/playing-cards.14f2b033.svg"
+  },
+  {
+    "revision": "21f5c3341370d60626a567a49b2034ef",
+    "url": "./static/media/software.21f5c334.svg"
+  }
+]);
